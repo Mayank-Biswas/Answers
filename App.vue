b1299459251b41dec2160631947bca6356245b3e
Mayank Biswas <70990850+Mayank-Biswas@users.noreply.github.com>
2021-09-07 21:48:55 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <Table v-if="tableData" :theData="tableData" :config="config" />
+  </div>
+</template>
+
+<script>
+import Table from './components/Table'
+
+export default {
+  components: {
+        Table
+ },
+ data: () => ({
+     tableData: undefined,
+     config: [
+         {
+           key: 'id',
+           title: 'ID',
+         },
+         {
+           key: 'email-add',
+           title: 'Email address'
+         },
+         {
+           key: 'first_name',
+           title: 'First-Name'
+         },
+         {
+           key: 'last_name',
+           title: 'Last-Name'
+         },
+         {
+           key: 'photo',
+           title: 'Avatar'
+         }
+     ]
+   }),
+   mounted() { 
+    this.$axios.get('https://reqres.in/api/users?page=2')
+    .then(({data}) => {
+             this.tableData = data
+    })
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ Table.vue
@@ -0,0 +1,73 @@
+<template>
+<div class="awesome-table">
+    <table>
+      <thead>
+        <tr>
+          <th v-for="(obj,ind) in config" :key="ind">
+                {{obj.title}}
+          </th>
+        </tr>
+      </thead>
+      
+      <tbody>
+        <tr v-for="(row,index) in theData" :key="index">
+          
+          <td v-for="(obj,ind) in config" :key="ind">
+               {{ row[obj.key] }}
+          
+          <span v-if="obj.type === 'text'">{{ row[obj.key] }}</span>
+          
+          <figure v-if="obj.type === 'image'">
+              <img :src="row[obj.key]">
+          </figure>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+</div>
+</template>
+
+<script>
+export default {
+     props: ['theData','config']
+}
+</script>
+
+<!--CSS-->
+<style scope>
+ .awesome-table
+ {
+   border: 1px solid black;
+   border-radius: 5px;
+   color: rgb(61, 60, 60);
+   overflow: auto;
+ }
+ .figure
+ {
+   margin-block-start: 0;
+   margin-block-end: 0;
+   margin-inline-start: 0;
+   margin-inline-end: 0;
+ }
+ .img
+ {
+   border-radius: 50%;
+   overflow: hidden;
+ }
+ .table 
+{
+  width: 100%;
+}
+.th
+{
+  position: fixed;
+  top: 2;
+  padding: 10px 5px;
+  text-align: center;
+}
+.td
+{
+ padding: 10px 5px;
+ text-align: center;
+}
+</style>
